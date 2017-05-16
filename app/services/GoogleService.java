a2d180550ebf795f33bd22a30f689f383b4cbdc4
ZlofZ <tobias.larsson96@gmail.com>
2017-05-16 14:16:16 +0200

    The gathering of nearby points of interests does now work as intended, though it's not being saved to the database.

--- app/services/GoogleService.java
+++ app/services/GoogleService.java
@@ -1,23 +1,15 @@
 package services;
 
-import akka.dispatch.Foreach;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.RawValue;
 import models.accommodation.Address;
-
-import java.io.BufferedReader;
 
 import java.io.IOException;
 
 import play.Logger;
 import play.libs.Json;
 
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
@@ -35,18 +27,22 @@ public class GoogleService{
 	private final String NEARBY_SEARCH="nearbysearch/";
 	private final String TEXT_SEARCH="textsearch/";
 	
-	
 	private final String[] types={"gym", "grocery_or_supermarket", "convenience_store", "subway_station", "bar", "restaurant"};
 	
-	
 	private final String JSON="json?";
-	private final String LOCATION="location=59.3525362,17.9796305";
-	private final String RADIUS="radius=1000";
+	private final String LOCATION="location=";
+	private final String RADIUS="radius=2000";
 	
 	
 	private String DATA="NODATA";
 	
 	//=====================================GENERIC METHODS
+	
+	/**
+	 * Finds the Coordinates, from Google Places of a particular address
+	 * @param address - Must contain streetname, number and area
+	 * @return - the address passed in with it's coordinates added.
+	 */
 	public Address getCoordinates(Address address){
 		
 		String query="query="+address.streetName+"+"+address.streetNumber+"+"+address.area;
@@ -87,117 +83,94 @@ public class GoogleService{
 		return null;
 	}
 	
-	public ObjectNode gatherNearbyData(){
-		Logger.debug("In GATHERNEARBYDATA");
-		ObjectNode d=Json.newObject();
+	/**
+	 * Gathers the nearby points of interest to a address.
+	 * @param address - The address from which to find nearby interests.
+	 * @return - All the points of interests close to the specified address.
+	 */
+	public ObjectNode gatherNearbyData(Address address){
+		ObjectNode allData=Json.newObject();
 		
 		for(String type : types){
-			Logger.debug("Getting all "+type+"-data");
-			ArrayNode temp=d.putArray(type);
-			ArrayNode dd=getAllNearbyInterests(type);
-			for(JsonNode jn : dd.get(0)){
-				temp.add(jn);
+			ArrayNode typeData=allData.putArray(type);
+			ArrayNode tempData=getAllNearbyInterests(type, address);
+			for(JsonNode tempNode : tempData.get(0)){
+				typeData.add(tempNode);
 			}
 		}
-		int i=0;
-		do{
-			Logger.debug("in do/while i="+i);
-			String npt=null;
-			String type=types[i];
-			while(npt==null){
-				npt=nextPageTokens.remove(type);
-				if(npt==null&&i<5){
-					Logger.debug("npt is null, loop nr:"+i+", type is "+type);
-					i++;
-					type=types[i];
-				}else if(i==5)break;
-			}
-			Logger.debug("npt is not null, type is "+type);
-			//Logger.debug(PLACES_URL+NEARBY_SEARCH+JSON+"pagetoken="+npt+"&"+PLACES_KEY);
-			JsonNode temp=null;
-			try{
-				while(temp==null&&npt!=null){
-					Logger.debug("waiting for 500ms.");
-					TimeUnit.MILLISECONDS.sleep(500);
-					Logger.debug("temp is null");
-					temp=gatherData(PLACES_URL+NEARBY_SEARCH+JSON+"pagetoken="+npt+"&"+PLACES_KEY);
-				}
-			}catch(NullPointerException|InterruptedException e){Logger.error("THROW");}
-			Logger.debug("Temp is not null!");
-			//Logger.debug(temp.toString());
-			for(JsonNode nodde:temp.findValues("results")){
-				Logger.debug("adding results: "+nodde.toString());
-				
-				d.withArray(type).add(nodde);
-			}
-			if(isNextPageTrue(temp)){
-				String s=temp.findValue("next_page_token").textValue();
-				nextPageTokens.put(type,s);
-			}
-			if(i<types.length-1)i++;
-			else i=0;
-		}while(!nextPageTokens.isEmpty());
-		return d;
-	}
-	
-	private JsonNode api(){
-		String urlString=PLACES_URL+NEARBY_SEARCH+JSON+LOCATION+"&"+RADIUS+"&"+PLACES_KEY;
-		Logger.debug("The apiurl is: "+urlString);
-		gatherData(urlString);
-		return null;
+		allData=findAdditionalData(allData);
+		for(String type : types){
+			Logger.debug("There are "+allData.get(type).size()+" "+type+'s');
+		}
+		return allData;
 	}
 	
 	/**
 	 * Executes gathering of data.
 	 */
 	public JsonNode gather(){
-		Logger.debug("STARTING API DATA GATHERING");
-		JsonNode data=api();
-		Logger.debug(data.findValue("name").toString());
-		return data;
+		return gatherData(PLACES_URL+TEXT_SEARCH+JSON+LOCATION+"&"+PLACES_KEY);
 	}
 	//==================================ADDITIONALDATAMETHODS
 	
-	private ArrayNode findAdditionalResults(String nextPageToken){
-		Logger.debug("Gathering Additional DATA");
-		//Logger.debug("NXTPGETOKN: "+nextPageToken);
-		String urlString=PLACES_URL+NEARBY_SEARCH+JSON+"pagetoken="+nextPageToken+"&"+PLACES_KEY;
-		//Logger.debug("URL: "+urlString);
-		JsonNode tempNode=null;
-		ArrayNode test=Json.newArray();
+	/**
+	 * Finds the next page data from from the first nextPageToken, if there is any
+	 * @param allData - the Root node the additional data is going to be added to
+	 * @return - @allData with the new data added
+	 */
+	private ObjectNode findAdditionalData(ObjectNode allData){
+		JsonNode tempDataNode=null;
+		String status="INVALID_REQUEST";
 		
-		tempNode=gatherData(urlString);
-		Logger.debug(tempNode.toString());
-		List<JsonNode>tempList=tempNode.findValues("results");
-		Logger.debug("size: "+tempList.size());
-		for(JsonNode tem:tempList){
-			Logger.debug("adding:"+ tem.get("name")+" to the array");
-			test.add(tem);
-		}
-		
-		if(isNextPageTrue(tempNode)){
-			test.addAll(findAdditionalResults(tempNode.findValue("next_page_token").toString()));
-		}
-		
-		Logger.debug("returning data gathered: "+test.size());
-		return test;
+		int i=0;
+		do{
+			String nextPageToken=null;
+			String type=types[i];
+			while(nextPageToken==null){
+				nextPageToken=nextPageTokens.remove(type);
+				if(nextPageToken==null&&i<5){
+					i++;
+					type=types[i];
+				}else if(i==5)break;
+			}
+			
+			try{
+				while(status.contentEquals("INVALID_REQUEST")|tempDataNode==null&&nextPageToken!=null){
+					TimeUnit.MILLISECONDS.sleep(500);
+					tempDataNode=gatherData(PLACES_URL+NEARBY_SEARCH+JSON+"pagetoken="+nextPageToken+"&"+PLACES_KEY);
+					status=tempDataNode.findValue("status").asText();
+				}
+			}catch(NullPointerException|InterruptedException e){Logger.error("THROW");}
+			
+			for(JsonNode tempNode:tempDataNode.findValues("results").get(0)){
+				ArrayNode typeList = (ArrayNode) allData.findValue(type);
+				typeList.add(tempNode);
+			}
+			if(isNextPageTrue(tempDataNode)){
+				String newNextPageToken=tempDataNode.findValue("next_page_token").textValue();
+				nextPageTokens.put(type,newNextPageToken);
+			}
+			if(i<types.length-1)i++;
+			else i=0;
+		}while(!nextPageTokens.isEmpty());
+		return allData;
 	}
 	
-	private ArrayNode getAllNearbyInterests(String type){
-		String urlString = PLACES_URL+NEARBY_SEARCH+JSON+LOCATION+"&"+RADIUS+"&"+"type="+type+"&"+PLACES_KEY;
+	private ArrayNode getAllNearbyInterests(String type, Address address){
+		String urlString = PLACES_URL+NEARBY_SEARCH+JSON+LOCATION+address.latitude+","+address.longitude+"&"+RADIUS+"&"+"type="+type+"&"+PLACES_KEY;
 		Logger.debug("Gathering "+type+"'s");
-		ArrayNode arno=Json.newArray();
+		ArrayNode nearbyInterestList=Json.newArray();
 		
 		JsonNode dataOfType=gatherData(urlString);
-		for(JsonNode d:dataOfType.findValues("results")){
-			arno.add(d);
+		for(JsonNode tempNode:dataOfType.findValues("results")){
+			nearbyInterestList.add(tempNode);
 		}
 		if(isNextPageTrue(dataOfType)){
 			Logger.debug("NextPageToken of "+type+" added to list.");
 			nextPageTokens.put(type,dataOfType.findValue("next_page_token").textValue());
 		}
 		
-		return arno;
+		return nearbyInterestList;
 	}
 	//==================================HELPERMETHODS
 	
@@ -261,65 +234,6 @@ public class GoogleService{
 		Logger.debug("number of subway_stations:        "+nearbyInterests.findValue("subway_station").findValue("results").size());
 		Logger.debug("number of bars:                   "+nearbyInterests.findValue("bar").findValue("results").size());
 		Logger.debug("number of restaurants:            "+nearbyInterests.findValue("restaurant").findValue("results").size());
-	}
-	
-	//=================================DEPRECATED
-	@Deprecated
-	private ArrayNode doApiCalll(URL url) throws IOException{
-		Logger.debug("in doApiCalll");
-		
-		StringBuilder content=new StringBuilder();
-		BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(url.openStream())); //reads all data from the url to a reader
-		String line;
-		while((line=bufferedReader.readLine())!=null) //appending every line of data that the reader has gathered to a string.
-		{
-			content.append(line);
-		}
-		bufferedReader.close();
-		JsonNode n = Json.parse(content.toString());
-		ArrayNode data= Json.newArray().addAll(n.findValues("results"));
-		Logger.debug("DATANODEADADA:"+data.toString());
-		return data;
-	}
-	
-	@Deprecated
-	private ArrayNode gatherDataa(String s){
-		Logger.debug("in gatherData");
-		try{
-			return doApiCalll(new URL(s));
-		}catch(MalformedURLException e){
-			e.printStackTrace();
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	@Deprecated
-	public JsonNode findNearbyInterests(){
-		Logger.debug("In findNearby");
-		String urlString;
-		ObjectNode nearbyInterests=null;
-		ArrayNode tempNode=null;
-		for(String t : types){
-			Logger.debug("finding "+t+"'s");
-			urlString=PLACES_URL+NEARBY_SEARCH+JSON+LOCATION+"&"+RADIUS+"&"+"type="+t+"&"+PLACES_KEY;
-			if(nearbyInterests==null){
-				nearbyInterests=new ObjectNode(new JsonNodeFactory(false));
-			}
-			for(JsonNode o : gatherData(urlString)){
-				tempNode.add(o);
-			}
-			
-			if(!tempNode.findValue("next_page_token").isNull()){
-				findAdditionalResults(tempNode.findValue("next_page_token").toString());
-			}
-			
-			nearbyInterests.set(t, tempNode);
-			Logger.debug(nearbyInterests.findValues(t).toString());
-		}
-		logInterests(nearbyInterests);
-		return nearbyInterests;
 	}
 	
 }
